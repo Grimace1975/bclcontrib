86c1173fdb34bab02a1f1a51da5b9a038db4aa4b
Moreys <Moreys@localhost>
2010-07-06 19:51:21 -0500

    Added DataType Validator

---
+++ Core/System.ComponentModelEx/ComponentModel/DataAnnotations/EmailValidation.cs
@@ -0,0 +1,17 @@
+﻿using System.Primitives.DataTypes;
+
+namespace System.ComponentModel.DataAnnotations
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    public class DataTypeValidation : ValidationAttribute
+    {
+        public DataTypeValidation(DataTypeParserBase parser)
+        {
+        }
+
+        public override bool IsValid(object value)
+        {
+            return true;
+        }
+    }
+}
