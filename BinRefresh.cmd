13f94100889931b784532b2fd6a3cccc43b62da7
Moreys <Moreys@localhost>
2010-07-29 10:28:25 -0500

    Added System.Web.Hosting

--- BinRefresh.cmd
+++ BinRefresh.cmd
@@ -15,6 +15,7 @@ xcopy Core\System.CoreEx\bin\Release\System.CoreEx.* bin\ /Q /R /H /Y
 xcopy Core\System.DataEx\bin\Release\System.DataEx.* bin\ /Q /R /H /Y
 xcopy Extents\Digital.Cms\bin\Release\Digital.Cms.* bin\ /Q /R /H /Y
 xcopy Services\System.ServiceModelEx\bin\Release\System.ServiceModelEx.* bin\ /Q /R /H /Y
+xcopy Web\System.Web.Hosting\bin\Release\System.Web.Hosting.* bin\ /Q /R /H /Y
 xcopy Web\System.Web.MvcEx\bin\Release\System.Web.MvcEx.* bin\ /Q /R /H /Y
 xcopy Web\System.WebEx\bin\Release\System.WebEx.* bin\ /Q /R /H /Y
 xcopy INTEGRATE_\WEB\System.WebEx.Integrate\bin\Release\System.WebEx.Integrate.* bin\INTEGRATE_\ /Q /R /H /Y
@@ -35,6 +36,7 @@ xcopy Core\System.CoreEx\bin\Debug\System.CoreEx.* bin\Debug\ /Q /R /H /Y
 xcopy Core\System.DataEx\bin\Debug\System.DataEx.* bin\Debug\ /Q /R /H /Y
 xcopy Extents\Digital.Cms\bin\Debug\Digital.Cms.* bin\Debug\ /Q /R /H /Y
 xcopy Services\System.ServiceModelEx\bin\Debug\System.ServiceModelEx.* bin\Debug\ /Q /R /H /Y
+xcopy Web\System.Web.Hosting\bin\Debug\System.Web.Hosting.* bin\Debug\ /Q /R /H /Y
 xcopy Web\System.Web.MvcEx\bin\Debug\System.Web.MvcEx.* bin\Debug\ /Q /R /H /Y
 xcopy Web\System.WebEx\bin\Debug\System.WebEx.* bin\Debug\ /Q /R /H /Y
 xcopy INTEGRATE_\WEB\System.WebEx.Integrate\bin\Debug\System.WebEx.Integrate.* bin\Debug\INTEGRATE_\ /Q /R /H /Y
@@ -42,6 +44,6 @@ xcopy INTEGRATE_\WEB\System.WebEx.Integrate\bin\Debug\System.WebEx.Integrate.* b
 ::
 robocopy Bin\Debug\ "C:\_APPLICATION\AFIWEB\Library_\Reference Assemblies\BclContrib" /MIR /TEE /NJH /NJS
 ::
-::robocopy Bin\ "C:\_APPLICATION\CITYOPWEB\Library_\Reference Assemblies\BclContrib" /MIR /TEE /NJH /NJS
+robocopy Bin\ "C:\_APPLICATION\CITYOPWEB\Library_\Reference Assemblies\BclContrib" /MIR /TEE /NJH /NJS
 ::robocopy Bin\Debug\ "C:\_APPLICATION\CITYOPWEB\Library_\Reference Assemblies\BclContrib" /MIR /TEE /NJH /NJS
 
