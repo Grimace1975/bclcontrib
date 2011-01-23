8eec573da960740e1255d5088f59b5d4a891eb4b
Sky Morey <Sky Morey@localhost>
2011-01-23 10:31:27 -0600

    Updates

--- SCRIPTSHARP_/ScriptApp.Tester/Content/Scripts/ScriptApp.Home.debug.js
+++ SCRIPTSHARP_/ScriptApp.Tester/Content/Scripts/ScriptApp.Home.debug.js
@@ -15,15 +15,10 @@ ScriptApp.Home._homePage = function ScriptApp_Home__homePage() {
 
 ScriptApp.Home._homePage.registerClass('ScriptApp.Home._homePage');
 (function () {
-    
-    window.atob('This is here');
-
-    var testFile = new SystemEx.IO.FileInfo('/Test/This');
-    alert(testFile.get_path());
 })();
 
 }
-ss.loader.registerScript('ScriptApp.Home', ['Script.WebEx'], executeScript);
+ss.loader.registerScript('ScriptApp.Home', [], executeScript);
 })();
 
 //! This script was generated using Script# v0.6.1.0
--- SCRIPTSHARP_/ScriptApp.Tester/Content/Scripts/ScriptApp.Home.js
+++ SCRIPTSHARP_/ScriptApp.Tester/Content/Scripts/ScriptApp.Home.js
@@ -1,6 +1,6 @@
 // ScriptApp.Home.js
 (function(){function executeScript(){
 Type.registerNamespace('ScriptApp.Home');ScriptApp.Home._HomePage=function(){}
-ScriptApp.Home._HomePage.registerClass('ScriptApp.Home._HomePage');(function(){window.atob('This is here');var $0=new SystemEx.IO.FileInfo('/Test/This');alert($0.get_path());})();
+ScriptApp.Home._HomePage.registerClass('ScriptApp.Home._HomePage');(function(){})();
 }
-ss.loader.registerScript('ScriptApp.Home',['Script.WebEx'],executeScript);})();// This script was generated using Script# v0.6.1.0
+ss.loader.registerScript('ScriptApp.Home',[],executeScript);})();// This script was generated using Script# v0.6.1.0
