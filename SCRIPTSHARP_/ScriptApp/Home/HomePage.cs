fbc2ffa71514121b47765547791de63c40ad5906
Sky Morey <Sky Morey@localhost>
2011-01-23 10:31:27 -0600

    Updates

--- SCRIPTSHARP_/ScriptApp/Home/HomePage.cs
+++ SCRIPTSHARP_/ScriptApp/Home/HomePage.cs
@@ -10,15 +10,13 @@ namespace ScriptApp.Home
     {
         static HomePage()
         {
-            LocalStorage["Key"] = "Value";
+            //CanvasElement x = CanvasElement
+            //CanvasElement c2D = x.GetContext("2d");
 
 
-            WindowEx.Atob("This is here");
-
-            FileInfo testFile = new FileInfo("/Test/This");
-            Script.Alert(testFile.Path);
+            //FileInfo testFile = new FileInfo("/Test/This");
+            //Script.Alert(testFile.Path);
             //testFile.CreateNewFile();
-            
             //foreach (FileInfo file in FileInfo.Root.ListFiles())
             //    Script.Alert(file.Path);
             //Script.Alert("Done");
