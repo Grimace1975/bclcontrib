dded7d230aa97907ea6678f0c936fa99dff02b68
Sky Morey <Sky Morey@localhost>
2011-02-06 16:58:26 -0600

    Updates

--- SCRIPTSHARP_/Script.WebEx/SystemEx/Timer.cs
+++ SCRIPTSHARP_/Script.WebEx/SystemEx/Timer.cs
@@ -1,35 +1,35 @@
-﻿#if !CODE_ANALYSIS
-namespace System
-#else
-using System;
-namespace SystemEx
-#endif
-{
-    #if CODE_ANALYSIS
-    public static class Timer
-    {
-        public static int Milliseconds()
-        {
-            return 0;
-        }
-    }
-#else
-    public static class Timer
-    {
-        public static int curtime = 0;
-        private static long baseTime = DateTime.Now.Ticks;
+﻿//#if !CODE_ANALYSIS
+//namespace System
+//#else
+//using System;
+//namespace SystemEx
+//#endif
+//{
+//    #if CODE_ANALYSIS
+//    public static class Timer
+//    {
+//        public static int Milliseconds()
+//        {
+//            return 0;
+//        }
+//    }
+//#else
+//    public static class Timer
+//    {
+//        public static int curtime = 0;
+//        private static long baseTime = DateTime.Now.Ticks;
 
-        public static int Milliseconds()
-        {
-            long time = DateTime.Now.Ticks;
-            long delta = time - baseTime;
-            unchecked
-            {
-                if (delta < 0)
-                    delta += long.MaxValue + 1;
-            }
-            return curtime = (int)(delta);
-        }
-    }
-#endif
-}
+//        public static int Milliseconds()
+//        {
+//            long time = DateTime.Now.Ticks;
+//            long delta = time - baseTime;
+//            unchecked
+//            {
+//                if (delta < 0)
+//                    delta += long.MaxValue + 1;
+//            }
+//            return curtime = (int)(delta);
+//        }
+//    }
+//#endif
+//}
